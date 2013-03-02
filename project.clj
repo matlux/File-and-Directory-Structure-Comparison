efc5ffd166695aacbaea82f561bf977a02749796
Mathieu Gauthron <mathieu@matlux.net>
2013-03-02 18:03:43 +0000

    moved onto clojure 1.4

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject cl-file-comparator "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [
-                 [org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure "1.4.0"]
                  [clj-ssh "0.3.0"]
                  [org.clojure/tools.cli "0.2.1"]
                  [slacker "0.4.0"]
