19777e3525aa8d514940ca9bebe90347acf44054
Mathieu Gauthron <mathieu@matlux.net>
2013-03-02 12:48:58 +0000

    fixed fileComp.sh script

--- fileComp.sh
+++ fileComp.sh
@@ -7,4 +7,4 @@ APPHOME=`echo $TMPz | sed s:/bin::`
 echo $APPHOME
 
 
-java -jar $APPHOME/cl-file-comparator-1.0.0-SNAPSHOT-standalone.jar $*
+java -jar $APPHOME/target/cl-file-comparator-1.0.0-SNAPSHOT-standalone.jar $*
